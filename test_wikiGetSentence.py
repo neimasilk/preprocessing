763951045c67b60e7a2b2bbf0186697b8b7bbb22
mukhlis <neimasilk@gmail.com>
2018-10-16 10:10:21 +0800

    generate 2 million indonesian sentences

---
+++ test_wikiGetSentence.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestWikiGetSentence(TestCase):
+    def test_generate_id(self):
+        # test wether it generates max_len and size
+        # generate indonesia sentences with max lengh of sentences max_len and count of sentences size
+        # examples : sentences = generate_id(80, 5)
+        # it should generates sentences with max sentences lenght of 80 with 5 sentences
+        #  sentences = ['kalimat 1', 'kalimat 2', 'kalimat 3', 'kalimat 4', 'kalimat 5']
+        self.fail()
