7d018255ee90c49d57f8d4196c1fd83b739f1859
mukhlis <neimasilk@gmail.com>
2018-10-17 11:42:21 +0800

    buat proxy

--- proxytrans.py
+++ proxytrans.py
@@ -1,9 +1,28 @@
 import urllib.request as urllib2
 import goslate
 
-proxy_handler = urllib2.ProxyHandler({"http" : "http://173.161.17.93:44251"})
-proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
-                                    urllib2.HTTPSHandler(proxy_handler))
 
-gs_with_proxy = goslate.Goslate(opener=proxy_opener)
-translation = gs_with_proxy.translate("hello world", "de")
+with open('proxies.txt', "r") as f:
+    array = []
+    for line in f:
+        array.append(line)
+    proxies = []
+for alamat in array:
+    proxies.append('http://'+alamat.split()[-1].replace('>',''))
+
+
+del array
+
+for proxy in proxies:
+    try:
+        proxy_handler = urllib2.ProxyHandler({"http" : proxy})
+        proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
+                                        urllib2.HTTPSHandler(proxy_handler))
+        gs_with_proxy = goslate.Goslate(opener=proxy_opener)
+        translation = gs_with_proxy.translate("hello world", "de")
+    except:
+        print(proxy)
+        continue
+    else:
+        print(translation)
+        break
