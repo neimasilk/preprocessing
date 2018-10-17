fcf9434c2d080955dcf8ff3b6c250177ed630ff2
mukhlis <neimasilk@gmail.com>
2018-10-17 12:20:38 +0800

    buat proxy

--- proxytrans.py
+++ proxytrans.py
@@ -1,5 +1,6 @@
 import urllib.request as urllib2
-import goslate
+import googletrans
+
 
 
 with open('proxies.txt', "r") as f:
@@ -13,13 +14,36 @@ for alamat in array:
 
 del array
 
+
 for proxy in proxies:
+
+    try:
+        prox_dict = {"http" : proxy}
+        translator = googletrans.Translator(service_urls=[
+            'translate.google.cn',
+            'translate.google.co.kr',
+        ], proxies=prox_dict)
+        item = "ini adalah percobaan"
+        translation = translator.translate(item, 'en', 'id').text
+
+    except:
+        print(proxy)
+        continue
+    else:
+        print(translation)
+        break
+
+
+
+# for proxy in proxies:
     # try:
-        proxy_handler = urllib2.ProxyHandler({"http" : proxy})
-        proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
-                                        urllib2.HTTPSHandler(proxy_handler))
-        gs_with_proxy = goslate.Goslate(opener=proxy_opener)
-        translation = gs_with_proxy.translate("hello world", "de")
+
+    #     proxy_handler = urllib2.ProxyHandler({"http" : proxy})
+    #     proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
+    #                                     urllib2.HTTPSHandler(proxy_handler))
+    #     urllib2.install_opener(proxy_opener)
+    #     gs_with_proxy = goslate.Goslate(opener=proxy_opener)
+    #     translation = gs_with_proxy.translate("hello world", "de")
     # except:
     #     print(proxy)
     #     continue
