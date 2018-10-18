925fd1f01e5802c2de640a41711150ffbd2ebb44
mukhlis <neimasilk@gmail.com>
2018-10-18 13:40:58 +0800

    buat proxy

--- coba_rotasi_deque.py
+++ coba_rotasi_deque.py
@@ -3,5 +3,5 @@ import shlex
 
 subprocess.call(shlex.split(
     "proxybroker find --types HTTP --lvl High --countries US CA FR GB DE SG --strict -l 100 -o ./proxies.txt"))
-subprocess.call(shlex.split("mv working_proxy.txt working_proxy%s.txt" % idnya))
+subprocess.call(shlex.split("mv working_proxy.txt working_proxy%s.txt" % 100))
 subprocess.call(shlex.split("python proxy_finder.py"))
