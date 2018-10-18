e4d4ac98fcc2dc25a1d94f4aa9c3248dea5681ee
mukhlis <neimasilk@gmail.com>
2018-10-18 13:37:26 +0800

    buat proxy

--- coba_rotasi_deque.py
+++ coba_rotasi_deque.py
@@ -1,6 +1,7 @@
 import subprocess
 import shlex
 
-r = subprocess.call(shlex.split("python wiki_get_sentence.py"))
-
-print("selesai")
+subprocess.call(shlex.split(
+    "proxybroker find --types HTTP --lvl High --countries US CA FR GB DE SG --strict -l 100 -o ./proxies.txt"))
+subprocess.call(shlex.split("mv working_proxy.txt working_proxy%s.txt" % idnya))
+subprocess.call(shlex.split("python proxy_finder.py"))
