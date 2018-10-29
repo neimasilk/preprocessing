8d2c2799b917d0531c2a12d78abe11d36db12617
mukhlis <neimasilk@gmail.com>
2018-10-29 10:37:03 +0800

    zhcn_service

--- translate_zhcn_serviceurl.py
+++ translate_zhcn_serviceurl.py
@@ -16,7 +16,7 @@ def translate_service(text, source, target, service=''):
     return translation
 
 
-filepath = 'mandarin_sentences_10.db'
+filepath = 'mandarin_sentences_1000000.db'
 try:
     db_connection = sqlite3.connect(filepath)
     db_cur = db_connection.cursor()
@@ -54,42 +54,30 @@ while True:
     break
 
 
-# for id in textnya:
-#     if (id[2] == None) or (id[3] == None):
-#         idnya = id[0]
-#         teks = id[3]
-#         while True:
-#             try:
-#                 if (id[2] == None):
-#                     artinya = translate_proxy(teks, 'zh-CN', 'en', proksi)
-#                 else:
-#                     artinya = id[2]
-#
-#                 if (id[1] == None):
-#                     artiid = translate_proxy(artinya, 'en', 'id', proksi)
-#                 else:
-#                     artiid = id[1]
-#             except Exception as e:
-#                 print(str(e))
-#                 wproxies.popleft()
-#                 if len(wproxies)==0:
-#                     print("proxy habis!")
-#                     subprocess.call(shlex.split("proxybroker find --types HTTP --lvl High --countries US CA FR GB DE SG --strict -l 300 -o ./proxies.txt"))
-#                     subprocess.call(shlex.split("mv working_proxy.txt working_proxy%s.txt" % idnya))
-#                     subprocess.call(shlex.split("python proxy_finder.py"))
-#                     # exit()
-#                     with open('working_proxy.txt', "r") as f:
-#                         for line in f:
-#                             wproxies.append(line)
-#                 proksi = extract_proxy(wproxies[0])
-#                 print(proksi)
-#
-#                 continue
-#             break
-#         # wproxies.rotate(1)
-#         # proksi = extract_proxy(wproxies[0])
-#         db_cur.execute(sql, [artinya, artiid, idnya])
-#         db_connection.commit()
-#         print(idnya)
-#
-# db_connection.close()
+for id in textnya:
+    if (id[2] == None) or (id[3] == None):
+        idnya = id[0]
+        teks = id[3]
+        while True:
+            try:
+                if (id[2] == None):
+                    artinya = translate_service(teks, 'zh-CN', 'en', servis)
+                else:
+                    artinya = id[2]
+
+                if (id[1] == None):
+                    artiid = translate_service(artinya, 'en', 'id', servis)
+                else:
+                    artiid = id[1]
+            except Exception as e:
+                print(str(e))
+                print(wservice[0])
+                wservice.rotate(1)
+                continue
+            break
+        wservice.rotate(1)
+        db_cur.execute(sql, [artinya, artiid, idnya])
+        db_connection.commit()
+        print(idnya)
+
+db_connection.close()
