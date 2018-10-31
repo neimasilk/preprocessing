7a5ca6b4221de30002ad15bce34fc1301c3311fc
mukhlis <neimasilk@gmail.com>
2018-10-31 08:21:04 +0800

    zhcn_service

--- coba_rotasi_deque.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import shlex
-
-subprocess.call(shlex.split(
-    "proxybroker find --types HTTP --lvl High --countries US CA FR GB DE SG --strict -l 100 -o ./proxies.txt"))
-# subprocess.call(shlex.split("mv working_proxy.txt working_proxy%s.txt" % 100))
-subprocess.call(shlex.split("python proxy_finder.py"))
